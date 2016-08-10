90849b4d630370920d5f045e9088576755361876
Abdeldjalil <abdeldjalil.benkoussas@gmail.com>
2016-08-10 15:06:14 +0200

    setup structure of test with karma js

---
+++ spec/fizzBuzz.spec.js
@@ -0,0 +1,11 @@
+var fizzBuzz = require('../lib/fizzBuzz');
+
+describe("Test fizzBuzz", function() {
+	
+	it("should print ", function() {
+		expect(fizzBuzz).toEqual('fizzBuzz');
+	})
+	
+	
+	
+});

3ab9097f57d995acc0bd8e54b4ec3a92f0669278
Abdeldjalil <abdeldjalil.benkoussas@gmail.com>
2016-08-12 23:42:33 +0200

    integrate version 3

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var fizzBuzz  = require('./lib/fizzBuzz.js').Program;
 var params = process.argv;
 // Program launched with  2 parametres 
 if(params[3]){
-	console.log("il y'a deux paramaters !");
+	console.log(fizzBuzz.run(3,params[2], params[3]));
+} else {
+	console.log(fizzBuzz.run(2, 3, 5));
 }
--- spec/fizzBuzz.spec.js
+++ spec/fizzBuzz.spec.js
@@ -11,29 +11,38 @@ describe("Test fizzBuzz", function() {
 	});
 
 	it(" -- test versions 1 -- should return 100 numbers", function(){
-		expect(fizzBuzz.run(1).length).toEqual(100);
+		expect(fizzBuzz.run(1 , 3, 5).length).toEqual(100);
 	});
 
 	it(" -- test version 1 -- should return Fizz", function(){
-		expect(fizzBuzz.run(1)[2]).toEqual('Fizz');
+		expect(fizzBuzz.run(1, 3, 5)[2]).toEqual('Fizz');
+		expect(fizzBuzz.run(1, 3, 5)[5]).toEqual('Fizz');
+		expect(fizzBuzz.run(1, 3, 5)[8]).toEqual('Fizz');
+		expect(fizzBuzz.run(1, 3, 5)[11]).toEqual('Fizz');
 	});
 
 	it("-- test versions 1 -- should return Buzz", function(){
-		expect(fizzBuzz.run(1)[4]).toEqual('Buzz');
+		expect(fizzBuzz.run(1, 3, 5)[4]).toEqual('Buzz');
 	});
 
 	//Test first version
 	it("-- test versions 1 -- should return table [1..15] with Fizz instead of 3 and Buzz instead of multipales if 5" , function(){
-		expect(fizzBuzz.run(1).slice(0,15)).toEqual([  1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz']);
+		expect(fizzBuzz.run(1, 3, 5).slice(0,15)).toEqual([  1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz']);
 	});
 
 	//Test seconde version
 	it(" -- test versions 2 -- should return 100 numbers", function(){
-		expect(fizzBuzz.run(2).length).toEqual(100);
+		expect(fizzBuzz.run(2, 3, 5).length).toEqual(100);
 	});
 	 
 	it("-- test versions 2 --should return table [1..15] with fizzBuzz  instead if multiples of 3&5 ", function(){
-		expect(fizzBuzz.run(2).slice(0,15)).toEqual([  1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']);
+		expect(fizzBuzz.run(2, 3, 5).slice(0,15)).toEqual([  1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']);
+	});	
+
+	it("-- test versions 3 -- should return Fizz instead of multiples of 2 ", function(){
+		expect(fizzBuzz.run(3, 2, 5)[1]).toEqual('Fizz');
+		expect(fizzBuzz.run(3, 2, 5)[3]).toEqual('Fizz');
+		expect(fizzBuzz.run(3, 2, 5)[5]).toEqual('Fizz');
 	});	
 	
 });

b3c679fa34969a7daa8a1d37190e59d0a5e15df0
Abdeldjalil <abdeldjalil.benkoussas@gmail.com>
2016-08-12 18:53:40 +0200

    add node package manager + test command

---
+++ index.js
@@ -0,0 +1,7 @@
+var fizzBuzz  = require('./lib/fizzBuzz.js').Program;
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val);
+  var param1 = array[2];
+  var param2 = array[3];
+  
+});

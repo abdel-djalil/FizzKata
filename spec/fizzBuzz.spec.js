97c5c4566539b9b8c5ff1c217db82df22c5a3a66
Abdeldjalil <abdeldjalil.benkoussas@gmail.com>
2016-08-12 18:12:18 +0200

    add documentation for function

--- spec/fizzBuzz.spec.js
+++ spec/fizzBuzz.spec.js
@@ -1,7 +1,6 @@
 var fizzBuzz  = require('../lib/fizzBuzz').Program;
 
 describe("Test fizzBuzz", function() {
-
 	//test helper function isMultipleOf3 
 	it("should return true if number is multiples of 3", function(){
 		expect(fizzBuzz.isMultipleOf3(3)).toEqual(true)	;
@@ -19,19 +18,29 @@ describe("Test fizzBuzz", function() {
 		expect(fizzBuzz.isMultipleOf5(8)).toEqual(false);	;
 	});
 
-	it("should return Fizz", function(){
+	it(" -- test versions 1 -- should return 100 numbers", function(){
+		expect(fizzBuzz.version1().length).toEqual(100);
+	});
+
+	it(" -- test version 1 -- should return Fizz", function(){
 		expect(fizzBuzz.version1()[2]).toEqual('Fizz');
 	});
-	it("should return Buzz", function(){
+
+	it("-- test versions 1 -- should return Buzz", function(){
 		expect(fizzBuzz.version1()[4]).toEqual('Buzz');
 	});
+
 	//Test first version
-	it("should return table [1..15] with Fizz instead of 3 and Buzz instead of multipales if 5" , function(){
+	it("-- test versions 1 -- should return table [1..15] with Fizz instead of 3 and Buzz instead of multipales if 5" , function(){
 		expect(fizzBuzz.version1().slice(0,15)).toEqual([  1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz']);
 	});
-	//Test seconde version 
-	it("should return table [1..15] with fizzBuzz  instead if multiples of 3&5 ", function(){
-		expect(fizzBuzz.version2().slice(0,15)).toEqual([  1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']);
+
+	//Test seconde version
+	it(" -- test versions 2 -- should return 100 numbers", function(){
+		expect(fizzBuzz.version2().length).toEqual(100);
 	});
-		
+	 
+	it("-- test versions 2 --should return table [1..15] with fizzBuzz  instead if multiples of 3&5 ", function(){
+		expect(fizzBuzz.version2().slice(0,15)).toEqual([  1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']);
+	});	
 });

8b88a99d97132235673d594cbb0c0386e400fe24
Abdeldjalil <abdeldjalil.benkoussas@gmail.com>
2016-08-16 23:00:59 +0200

    handle v1 execution

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var Program  = require('./lib/fizzBuzz.js');
 var fizzBuzz = new Program();
 var params = process.argv;
-// Program launched with  rules 
+
+// if there is 3 params launch the V3
 if(params[3]){
 	console.log(fizzBuzz.run(3,params[2], params[3]));
-} else {
+
+}
+// if there is only one param with value equal to 1 launch v1
+else if (params.length == 3 && params[2] == "1"){
+	console.log(fizzBuzz.run(1, 3, 5));
+
+} 
+// if there is no params launch V2
+else if(!params[2]){
 	console.log(fizzBuzz.run(2, 3, 5));
+} else{
+	console.log("please check documentation on git to launch app!");
 }
